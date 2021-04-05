678c2a27f764ade54edc29f2a38b91e62080de11
Valenta019 <71689925+Valenta019@users.noreply.github.com>
2021-04-05 21:54:11 +0200

    Movement possible

--- images.g.ts
+++ images.g.ts
@@ -39,78 +39,6 @@ f f f f f f f f f f f f f f f f f f f f
 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
 `;
-            case "image3":
-            case "Black_checker":return img`
-. . . . f f f f f f f f f f f f . . . . 
-. . . f d d d d d d d d d d d d f . . . 
-. . f d d f f f f f f f f f f d d f . . 
-. f d d f f f f f f f f f f f f d d f . 
-f d d f f f f f f f f f f f f f f d d f 
-f d f f f f f f f f f f f f f f f f d f 
-f d f f f f f f f f f f f f f f f f d f 
-f d f f f f f f f f f f f f f f f f d f 
-f d f f f f f f f f f f f f f f f f d f 
-f d f f f f f f f f f f f f f f f f d f 
-f d d f f f f f f f f f f f f f f d d f 
-. f d d f f f f f f f f f f f f d d f . 
-. . f d d f f f f f f f f f f d d f . . 
-. . . f d d d d d d d d d d d d f . . . 
-. . . . f f f f f f f f f f f f . . . . 
-`;
-            case "image7":
-            case "White_checker":return img`
-. . . . 1 1 1 1 1 1 1 1 1 1 1 1 . . . . 
-. . . 1 b b b b b b b b b b b b 1 . . . 
-. . 1 b b 1 1 1 1 1 1 1 1 1 1 b b 1 . . 
-. 1 b b 1 1 1 1 1 1 1 1 1 1 1 1 b b 1 . 
-1 b b 1 1 1 1 1 1 1 1 1 1 1 1 1 1 b b 1 
-1 b 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 b 1 
-1 b 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 b 1 
-1 b 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 b 1 
-1 b 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 b 1 
-1 b 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 b 1 
-1 b b 1 1 1 1 1 1 1 1 1 1 1 1 1 1 b b 1 
-. 1 b b 1 1 1 1 1 1 1 1 1 1 1 1 b b 1 . 
-. . 1 b b 1 1 1 1 1 1 1 1 1 1 b b 1 . . 
-. . . 1 b b b b b b b b b b b b 1 . . . 
-. . . . 1 1 1 1 1 1 1 1 1 1 1 1 . . . . 
-`;
-            case "image8":
-            case "Black_lady":return img`
-. . . . f f f f f f f f f f f f . . . . 
-. . . f d d d d d d d d d d d d f . . . 
-. . f d d f f f f f f f f f f d d f . . 
-. f d d f f f f f f f f f f f f d d f . 
-f d d f f 5 5 f f 5 5 f f 5 5 f f d d f 
-f d f f f 5 5 f f 5 5 f f 5 5 f f f d f 
-f d f f f 5 5 f f 5 5 f f 5 5 f f f d f 
-f d f f f 5 5 5 f 5 5 f 5 5 5 f f f d f 
-f d f f f 5 5 5 5 5 5 5 5 5 5 f f f d f 
-f d f f f 5 5 5 5 5 5 5 5 5 5 f f f d f 
-f d d f f 5 5 5 5 5 5 5 5 5 5 f f d d f 
-. f d d f f f f f f f f f f f f d d f . 
-. . f d d f f f f f f f f f f d d f . . 
-. . . f d d d d d d d d d d d d f . . . 
-. . . . f f f f f f f f f f f f . . . . 
-`;
-            case "image9":
-            case "White_lady":return img`
-. . . . 1 1 1 1 1 1 1 1 1 1 1 1 . . . . 
-. . . 1 b b b b b b b b b b b b 1 . . . 
-. . 1 b b 1 1 1 1 1 1 1 1 1 1 b b 1 . . 
-. 1 b b 1 1 1 1 1 1 1 1 1 1 1 1 b b 1 . 
-1 b b 1 1 5 5 1 1 5 5 1 1 5 5 1 1 b b 1 
-1 b 1 1 1 5 5 1 1 5 5 1 1 5 5 1 1 1 b 1 
-1 b 1 1 1 5 5 1 1 5 5 1 1 5 5 1 1 1 b 1 
-1 b 1 1 1 5 5 5 1 5 5 1 5 5 5 1 1 1 b 1 
-1 b 1 1 1 5 5 5 5 5 5 5 5 5 5 1 1 1 b 1 
-1 b 1 1 1 5 5 5 5 5 5 5 5 5 5 1 1 1 b 1 
-1 b b 1 1 5 5 5 5 5 5 5 5 5 5 1 1 b b 1 
-. 1 b b 1 1 1 1 1 1 1 1 1 1 1 1 b b 1 . 
-. . 1 b b 1 1 1 1 1 1 1 1 1 1 b b 1 . . 
-. . . 1 b b b b b b b b b b b b 1 . . . 
-. . . . 1 1 1 1 1 1 1 1 1 1 1 1 . . . . 
-`;
             case "image4":
             case "Selector_grn":return img`
 7 7 7 7 7 7 . . . . . . . . 7 7 7 7 7 7 
@@ -146,6 +74,96 @@ f d d f f 5 5 5 5 5 5 5 5 5 5 f f d d f
 2 . . . . . . . . . . . . . . . . . . 2 
 2 . . . . . . . . . . . . . . . . . . 2 
 2 2 2 2 2 2 2 . . . . . . . 2 2 2 2 2 2 
+`;
+            case "image3":
+            case "Black_checker":return img`
+. . . . . f f f f f f f f f f . . . . . 
+. . . . f d d d d d d d d d d f . . . . 
+. . . f d d f f f f f f f f d d f . . . 
+. . f d d f f f f f f f f f f d d f . . 
+. f d d f f f f f f f f f f f f d d f . 
+. f d f f f f f f f f f f f f f f d f . 
+. f d f f f f f f f f f f f f f f d f . 
+. f d f f f f f f f f f f f f f f d f . 
+. f d f f f f f f f f f f f f f f d f . 
+. f d f f f f f f f f f f f f f f d f . 
+. f d d f f f f f f f f f f f f d d f . 
+. . f d d f f f f f f f f f f d d f . . 
+. . . f d d f f f f f f f f d d f . . . 
+. . . . f d d d d d d d d d d f . . . . 
+. . . . . f f f f f f f f f f . . . . . 
+`;
+            case "image7":
+            case "White_checker":return img`
+. . . . . 1 1 1 1 1 1 1 1 1 1 . . . . . 
+. . . . 1 b b b b b b b b b b 1 . . . . 
+. . . 1 b b 1 1 1 1 1 1 1 1 b b 1 . . . 
+. . 1 b b 1 1 1 1 1 1 1 1 1 1 b b 1 . . 
+. 1 b b 1 1 1 1 1 1 1 1 1 1 1 1 b b 1 . 
+. 1 b 1 1 1 1 1 1 1 1 1 1 1 1 1 1 b 1 . 
+. 1 b 1 1 1 1 1 1 1 1 1 1 1 1 1 1 b 1 . 
+. 1 b 1 1 1 1 1 1 1 1 1 1 1 1 1 1 b 1 . 
+. 1 b 1 1 1 1 1 1 1 1 1 1 1 1 1 1 b 1 . 
+. 1 b 1 1 1 1 1 1 1 1 1 1 1 1 1 1 b 1 . 
+. 1 b b 1 1 1 1 1 1 1 1 1 1 1 1 b b 1 . 
+. . 1 b b 1 1 1 1 1 1 1 1 1 1 b b 1 . . 
+. . . 1 b b 1 1 1 1 1 1 1 1 b b 1 . . . 
+. . . . 1 b b b b b b b b b b 1 . . . . 
+. . . . . 1 1 1 1 1 1 1 1 1 1 . . . . . 
+`;
+            case "image8":
+            case "Black_lady":return img`
+. . . . . f f f f f f f f f f . . . . . 
+. . . . f d d d d d d d d d d f . . . . 
+. . . f d d f f f f f f f f d d f . . . 
+. . f d d f f f f f f f f f f d d f . . 
+. f d d f 5 5 f f 5 5 f f 5 5 f d d f . 
+. f d f f 5 5 f f 5 5 f f 5 5 f f d f . 
+. f d f f 5 5 f f 5 5 f f 5 5 f f d f . 
+. f d f f 5 5 5 f 5 5 f 5 5 5 f f d f . 
+. f d f f 5 5 5 5 5 5 5 5 5 5 f f d f . 
+. f d f f 5 5 5 5 5 5 5 5 5 5 f f d f . 
+. f d d f 5 5 5 5 5 5 5 5 5 5 f d d f . 
+. . f d d f f f f f f f f f f d d f . . 
+. . . f d d f f f f f f f f d d f . . . 
+. . . . f d d d d d d d d d d f . . . . 
+. . . . . f f f f f f f f f f . . . . . 
+`;
+            case "image9":
+            case "White_lady":return img`
+. . . . . 1 1 1 1 1 1 1 1 1 1 . . . . . 
+. . . . 1 b b b b b b b b b b 1 . . . . 
+. . . 1 b b 1 1 1 1 1 1 1 1 b b 1 . . . 
+. . 1 b b 1 1 1 1 1 1 1 1 1 1 b b 1 . . 
+. 1 b b 1 5 5 1 1 5 5 1 1 5 5 1 b b 1 . 
+. 1 b 1 1 5 5 1 1 5 5 1 1 5 5 1 1 b 1 . 
+. 1 b 1 1 5 5 1 1 5 5 1 1 5 5 1 1 b 1 . 
+. 1 b 1 1 5 5 5 1 5 5 1 5 5 5 1 1 b 1 . 
+. 1 b 1 1 5 5 5 5 5 5 5 5 5 5 1 1 b 1 . 
+. 1 b 1 1 5 5 5 5 5 5 5 5 5 5 1 1 b 1 . 
+. 1 b b 1 5 5 5 5 5 5 5 5 5 5 1 b b 1 . 
+. . 1 b b 1 1 1 1 1 1 1 1 1 1 b b 1 . . 
+. . . 1 b b 1 1 1 1 1 1 1 1 b b 1 . . . 
+. . . . 1 b b b b b b b b b b 1 . . . . 
+. . . . 1 1 1 1 1 1 1 1 1 1 1 . . . . . 
+`;
+            case "image6":
+            case "Green_tile":return img`
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
 `;
         }
         return null;
--- main.ts
+++ main.ts
@@ -4,15 +4,21 @@ namespace SpriteKind{
     export const Selector = SpriteKind.create()
     export const BlackChecker = SpriteKind.create()
     export const WhiteChecker = SpriteKind.create()
-} 
+    //export const BlackLady = SpriteKind.create()
+    //export const WhiteLady = SpriteKind.create()
+}
 
 scene.setBackgroundColor(6)
+
 // Globals
-let checkertiles = [] //Chessboard
+let checkertiles = [ [] as Sprite[]] //Chessboard
 let selector = sprites.create(assets.image`Selector_grn`,SpriteKind.Selector)
+selector.z = 10
 let black = {checkers: [] as Sprite[], startSide:0}
 let white = {checkers: [] as Sprite[], startSide:1}
 
+let selected:Sprite = null
+let ply = "white"
 
 
 
@@ -27,7 +33,7 @@ for (let i = 0; i < 8; i++) {
         if (colourCounter%2 == 0) {
          checkertilerow[j] = sprites.create(assets.image`Black_tile`,SpriteKind.BlackTile)}
          else {
-                     checkertilerow[j] = sprites.create(assets.image`White_tile`,SpriteKind.WhiteTile)
+         checkertilerow[j] = sprites.create(assets.image`White_tile`,SpriteKind.WhiteTile)
          }
          colourCounter++
     }
@@ -105,35 +111,164 @@ let selectorPos = {x:checkertiles[0][0].x, y:checkertiles[0][0].y} //Position of
 { // ### Selector movement
 let tileWidth = checkertiles[0][0].width
 let tileHeight = checkertiles[0][0].height
+function UpdateSelectorPos(){
+    selector.setPosition(selectorPos["x"], selectorPos["y"])
+    //selector.z = 10
+
+}
+
 
 controller.left.onEvent(ControllerButtonEvent.Pressed, function() {
     if (selectorPos["x"] - tileWidth >= 0){
         selectorPos["x"] -= tileWidth
+        UpdateSelectorPos()
     }
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function() {
     if (selectorPos["x"] + tileWidth <= 160){
         selectorPos["x"] += tileWidth
+        UpdateSelectorPos()
     }
 })
 controller.up.onEvent(ControllerButtonEvent.Pressed, function() {
     if (selectorPos["y"] - tileHeight >= 0){
         selectorPos["y"] -= tileHeight
+        UpdateSelectorPos()
     }
 })
 controller.down.onEvent(ControllerButtonEvent.Pressed, function() {
     if (selectorPos["y"] + tileHeight <= 120){
         selectorPos["y"] += tileHeight
+        UpdateSelectorPos()
     }
 })
+
+UpdateSelectorPos()
 }
 
 
+{// ### Selector selecting
 
- // ### Think
+//Checker selecting
+sprites.onOverlap(SpriteKind.Selector, SpriteKind.BlackChecker, function(sprite: Sprite, otherSprite: Sprite) {
+    if (ply == "black"){
+        if (controller.A.isPressed()) {
+            Deselect()
+            selected = otherSprite
+            DetermineMoves(selected)
+        }
+    }
+})
+sprites.onOverlap(SpriteKind.Selector, SpriteKind.WhiteChecker, function(sprite: Sprite, otherSprite: Sprite) {
+    if (ply == "white"){
+        if (controller.A.isPressed()) {
+            Deselect()
+            selected = otherSprite           
+            DetermineMoves(selected)
+        }
+    }
+})
+
+//Target selecting & movement
+sprites.onOverlap(SpriteKind.Selector, SpriteKind.BlackTile, function(sprite: Sprite, otherSprite: Sprite) {
+     if(otherSprite.image.equals(assets.image`Green_tile`)){
+         selector.setImage(assets.image`Selector_red`)
+        if (controller.A.isPressed()) {
+            selected.setPosition(otherSprite.x, otherSprite.y)
+            Deselect()
+            if (ply =="white"){
+                ply = "black"
+            }
+            else
+            {ply = "white"}
+
+        }
+    }
+    else{
+        selector.setImage(assets.image`Selector_grn`)
+    }
+})
+
+    sprites.onOverlap(SpriteKind.Selector, SpriteKind.WhiteTile, function(sprite: Sprite, otherSprite: Sprite) {
+        selector.setImage(assets.image`Selector_grn`)
+    })
+
+
+ function Deselect(){
+    selected = null 
+    for (let i = 0; i < checkertiles.length; i++) {
+        for ( let j = 0; j < checkertiles[i].length; j++)
+        if (checkertiles[i][j].image.equals(assets.image`Green_tile`)){
+            checkertiles[i][j].setImage(assets.image`Black_tile`)
+        }
+    }
+ 
+}
+controller.B.onEvent(ControllerButtonEvent.Pressed, function(){
+    Deselect()
+})
+}
+
+{// ### Moves determination
+let lastTouchedBlack:Sprite = null
+const way = 1000
+const wayX = [way, -way, way, -way]
+const wayY = [way-100, way-100, -way+100, -way+100]
+
+    function DetermineMoves(forwhat:Sprite) {
+        if (forwhat != null){
+            for (let i=0;i<4;i++){
+                let projectile = sprites.createProjectileFromSprite(img`
+                . . . . . . . . . . . . . . . .
+                . . . . . . . . . . . . . . . .
+                . . . . . . . . . . . . . . . .
+                . . . . . . . . . . . . . . . .
+                . . . . . . . . . . . . . . . .
+                . . . . . . . . . . . . . . . .
+                . . . . . . . . . . . . . . . .
+                . . . . . . . f f . . . . . . .
+                . . . . . . . f f . . . . . . .
+                . . . . . . . . . . . . . . . .
+                . . . . . . . . . . . . . . . .
+                . . . . . . . . . . . . . . . .
+                . . . . . . . . . . . . . . . .
+                . . . . . . . . . . . . . . . .
+                . . . . . . . . . . . . . . . .
+                . . . . . . . . . . . . . . . .
+                `, selected, wayX[i], wayY[i])
+                //projectile.setFlag(SpriteFlag.Invisible, true)
+            }
+        }
+    }
+    // Selected tile ghosting
+    /*sprites.onOverlap(SpriteKind.Selector, SpriteKind.BlackTile, function(sprite: Sprite, otherSprite: Sprite) {
+        lastTouchedBlack = otherSprite
+        lastTouchedBlack.setFlag(SpriteFlag.Ghost, true)
+    })
+        sprites.onOverlap(SpriteKind.Selector, SpriteKind.WhiteTile, function(sprite: Sprite, otherSprite: Sprite) {
+        lastTouchedBlack.setFlag(SpriteFlag.Ghost, false)
+    })*/
+
+ // Ignore the tile you are on, ladies have infinite range
+    sprites.onOverlap(SpriteKind.Projectile, SpriteKind.BlackTile, function(sprite: Sprite, otherSprite: Sprite) {
+        if (!otherSprite.overlapsWith(selector)){
+            otherSprite.setImage(assets.image`Green_tile`)
+            if (!selected.image.equals(assets.image`White_lady`) || !selected.image.equals(assets.image`Black_lady`)){
+                sprite.destroy()
+            }
+        }
+        
+    })
+
+ // Occupied tiles can't be green
+    sprites.onOverlap(SpriteKind.BlackChecker, SpriteKind.BlackTile, function(sprite: Sprite, otherSprite: Sprite) {
+        otherSprite.setImage(assets.image`Black_tile`)
+    })
+    sprites.onOverlap(SpriteKind.WhiteChecker, SpriteKind.BlackTile, function(sprite: Sprite, otherSprite: Sprite) {
+        otherSprite.setImage(assets.image`Black_tile`)
+    })
+}
+
 game.onUpdate(function() {
-    selector.setPosition(selectorPos["x"], selectorPos["y"])
-    selector.z = 10
-
     
 })
