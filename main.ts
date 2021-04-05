cacc0677b8e25a93eebb96ad378ab041d9269700
Valenta019 <71689925+Valenta019@users.noreply.github.com>
2021-04-05 16:19:35 +0200

    array of tiles

--- main.ts
+++ main.ts
@@ -1,1 +1,32 @@
- 
+namespace SpriteKind{
+    export const BlackTile = SpriteKind.create()
+    export const WhiteTile = SpriteKind.create()
+} 
+
+
+scene.setBackgroundColor(1)
+let blacktiles = []
+for (let i = 0; i < 8; i++) {
+    let blacktilerow = []
+    for (let j = 0; j < 8; j++) {
+         blacktilerow[j] = sprites.create(img`
+             f f f f f f f f f f f f f f f f
+             f f f f f f f f f f f f f f f f
+             f f f f f f f f f f f f f f f f
+             f f f f f f f f f f f f f f f f
+             f f f f f f f f f f f f f f f f
+             f f f f f f f f f f f f f f f f
+             f f f f f f f f f f f f f f f f
+             f f f f f f f f f f f f f f f f
+             f f f f f f f f f f f f f f f f
+             f f f f f f f f f f f f f f f f
+             f f f f f f f f f f f f f f f f
+             f f f f f f f f f f f f f f f f
+             f f f f f f f f f f f f f f f f
+             f f f f f f f f f f f f f f f f
+             f f f f f f f f f f f f f f f f
+             f f f f f f f f f f f f f f f f
+         `,SpriteKind.BlackTile)
+    }
+    blacktiles[i] = blacktilerow
+}
